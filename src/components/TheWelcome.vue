37c7fc0f313a345a0cde0475540e160ca2f05327
meRiz <rizkycandi147@gmail.com>
2023-01-08 17:06:10 +0700

    This is a new commit for what I originally planned to be amended

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,38 @@
+<script>
+export default {
+  data() {
+    return {
+      listsuhu: [],
+    }
+  },
+  methods:{
+   
+   async ambildata() {
+     const response = await fetch("http://localhost:3000/getlist");
+     const data = await response.json();
+     this.listsuhu = data.sensor_suhu;
+     console.log( this.listsuhu[0] );
+   }
+ }
+
+}
+
+
+</script>
+
+
+<template>
+  <table class="p-10">
+    <tr>
+      <td>Waktu</td>
+      <td>suhu</td>
+    </tr>
+    <tr v-for="item in listsuhu">
+      <td>{{ item.waktu }}</td>
+      <td>{{ item.suhu }}</td>
+    </tr>
+  </table>
+  <div>
+    <button class="rounded hover:rounded-lg bg-blue-600 text-white" @click="ambildata()">Ambil Data</button>
+  </div>
+</template>
